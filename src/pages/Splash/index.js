21f4ecd0e845176ec71796336b11a8a8d76548d0
Indra PC <ndra2404@gmail.com>
2021-10-14 10:57:10 +0700

    ayoo bisa

--- src/pages/Splash/index.js
+++ src/pages/Splash/index.js
@@ -1,19 +1,22 @@
-import React, {useEffect} from 'react';
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
-import {BgsplashScreen} from '../../assets';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { BgsplashScreen } from "../../assets";
 
-const image = { uri: "https://reactjs.org/logo-og.png" };
-const Splash = ({navigation}) => {
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         navigation.replace("MainApp")
-    //     }, 5000);
-    // },[navigation])
+const Splash = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace("MainApp");
+    }, 3000);
+  }, [navigation]);
   return (
     <View style={styles.container}>
-        <ImageBackground source={BgsplashScreen} resizeMode="cover" style={styles.image}>
-            <Text style={styles.text}>Please Wait</Text>
-        </ImageBackground>
+      <ImageBackground
+        source={BgsplashScreen}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Please Wait</Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -21,12 +24,12 @@ const Splash = ({navigation}) => {
 export default Splash;
 
 const styles = StyleSheet.create({
-    image:{
-        flex: 1,
-        alignItems :'center',
-        justifyContent:'center'
-    },
-    container: {
+  image: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container: {
     flex: 1,
   },
 });
